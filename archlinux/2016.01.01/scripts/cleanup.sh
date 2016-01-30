12b11266f6f213ec65bc91f3d8366a74a1fc9bc1
Bram Swenson <bram@craniumisajar.com>
2016-01-30 18:16:23 -0500

    Cleanup orphaned packages in archlinux-2016.01.01 cleanup script

--- archlinux/2016.01.01/scripts/cleanup.sh
+++ archlinux/2016.01.01/scripts/cleanup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 PACMAN="$(which sudo | grep -v 'not found') $(which pacman) --quiet --noconfirm"
-NAME=$1
-
-sudo $PACMAN -Sc
 
 test ! -d /var/lib/cloud || sudo rm -rf /var/lib/cloud/*
-
-sudo bash -c "echo \"${NAME} - built with packer\" > /etc/build.info"
+sudo $PACMAN -Sc
+sudo $PACMAN -Rns $($PACMAN -Qtdq)
+sudo bash -c "echo \"${ATLAS_BUILD_SLUG}:${ATLAS_BUILD_GITHUB_COMMIT_SHA} - built with packer\" > /etc/build.info"
