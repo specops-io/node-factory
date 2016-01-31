59e40449d511639e18b0a2110de0df56a827aefc
Bram Swenson <bram@craniumisajar.com>
2016-01-30 19:59:45 -0500

    Ok, only simple scripting allowed in packer for archlinux-2016.01.01

--- archlinux/2016.01.01/scripts/create_vagrant_users.sh
+++ archlinux/2016.01.01/scripts/create_vagrant_users.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+SUDO="$(which sudo | grep -v 'not found')"
 
-./archlinux/2016.01.01/scripts/create_user.sh arch 'Archlinux Base User'
-./archlinux/2016.01.01/scripts/add_user_to_sudo_nopass.sh arch
+create_user() {
+  username="$1"
+  comment="$2"
+  echo "creating user $username with comment $comment"
+  $SUDO useradd -m -s /bin/bash -c $comment $username
+}
 
-./archlinux/2016.01.01/scripts/create_user.sh vagrant 'Vagrant User'
-./archlinux/2016.01.01/scripts/add_user_to_sudo_nopass.sh vagrant
+add_user_to_sudo_nopass() {
+  username="$1"
+  echo "adding user $username to nopass sudo"
+  $SUDO tee /etc/sudoers.d/$username << EOF
+$username ALL=(ALL) NOPASSWD: ALL
+EOF
+}
+
+create_user arch 'Archlinux Base User'
+add_user_to_sudo_nopass arch
+
+create_user vagrant 'Vagrant User'
+add_user_to_sudo_nopass vagrant
