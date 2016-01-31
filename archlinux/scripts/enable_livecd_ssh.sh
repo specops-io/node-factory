be879b47383949380d3886c80c770c7f8ae0fd8f
Bram Swenson <bram@craniumisajar.com>
2016-01-31 16:47:23 -0500

    Use vagrant user everwhere

--- archlinux/scripts/enable_livecd_ssh.sh
+++ archlinux/scripts/enable_livecd_ssh.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+PACMAN="$(which pacman) --quiet --noconfirm"
+test `which sudo` || $PACMAN -S sudo
+SUDO="$(which sudo)"
 
-echo -e "specops\nspecops" | passwd
+$SUDO useradd -m -s /bin/bash vagrant
+echo -e "vagrant\nvagrant" | $SUDO passwd vagrant
+
+$SUDO tee /etc/sudoers.d/vagrant << EOF
+Defaults env_keep += "SSH_AUTH_SOCK"
+vagrant ALL=(ALL) NOPASSWD: ALL
+EOF
+
 
 $SUDO systemctl start sshd.service
