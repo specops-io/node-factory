1881e73bc1323860fb060aa5f73d7fc9529e2202
Bram Swenson <bram@craniumisajar.com>
2016-01-31 20:55:00 -0500

    Add net-tools to base

--- archlinux/scripts/base.sh
+++ archlinux/scripts/base.sh
@@ -3,5 +3,5 @@ SUDO="$(which sudo | grep -v 'not found')"
 PACMAN="${SUDO} $(which pacman) --quiet --noconfirm"
 
 $PACMAN -Syy archlinux-keyring
-$PACMAN -Syyu
+$PACMAN -Suuy net-tools
 $SUDO pacman-db-upgrade
