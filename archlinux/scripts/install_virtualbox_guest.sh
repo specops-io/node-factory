0dc0d4042d0ff7b58641d8cdd9b0b26bf46c81cf
Bram Swenson <bram@craniumisajar.com>
2016-01-31 21:04:43 -0500

    Remove extra backslash

--- archlinux/scripts/install_virtualbox_guest.sh
+++ archlinux/scripts/install_virtualbox_guest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 SUDO="$(which sudo | grep -v 'not found')"
 PACMAN="${SUDO} $(which pacman) --quiet --noconfirm"
-guest_version="$($SUDO /usr/bin/pacman -Q virtualbox-guest-dkms | awk '{ print \$2 }' | cut -d'-' -f1)"
-kernel_version="$($SUDO /usr/bin/pacman -Q linux | awk '{ print \$2 }')-ARCH"
+guest_version="$($SUDO /usr/bin/pacman -Q virtualbox-guest-dkms | awk '{ print $2 }' | cut -d'-' -f1)"
+kernel_version="$($SUDO /usr/bin/pacman -Q linux | awk '{ print $2 }')-ARCH"
 
 # VirtualBox Guest Additions
 $PACMAN -S linux-headers virtualbox-guest-utils virtualbox-guest-dkms nfs-utils
@@ -13,7 +13,7 @@ vboxsf
 vboxvideo
 EOF
 
-$SUDO dkms install "vboxguest/\${guest_version}" -k "\${kernel_version}/x86_64"
+$SUDO dkms install "vboxguest/${guest_version}" -k "${kernel_version}/x86_64"
 $SUDO systemctl enable dkms.service
 $SUDO systemctl enable vboxservice.service
 $SUDO systemctl enable rpcbind.service
